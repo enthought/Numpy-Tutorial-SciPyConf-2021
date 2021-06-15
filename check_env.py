c7c0f8acad2d0c5d0a48dee0eefa91ab3238d941
Logan Thomas <logan.thomas005@gmail.com>
2021-06-15 11:47:27 -0500

    Fix: update check_env script to fall back to default matplotlib backend (#3)
    * Closes #2

--- check_env.py
+++ check_env.py
@@ -1,13 +1,9 @@
 """ Run this file to check your python installation.
 """
-
-import matplotlib
-matplotlib.use('Qt5Agg')
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-assert np.allclose(np.array([3.3], dtype='float32'), np.array([1.65], dtype='float32')*2)
+assert np.allclose(np.array([3.3], dtype="float32"), np.array([1.65], dtype="float32") * 2)
 fig, ax = plt.subplots()
 ax.scatter(x=[-3, -2, -1, 0, 1, 2, 3], y=[0, -1, -1.5, -1.75, -1.5, -1, 0])
 ax.scatter(x=[-1.5, 1.5], y=[2, 2], s=1000)
